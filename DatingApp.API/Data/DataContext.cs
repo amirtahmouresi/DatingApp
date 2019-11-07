d6655f5cb38a0742bcdc1b060dd99ff2290e45f5
unknown <amir.tahmouresi.1881@gmail.com>
2019-11-07 11:47:33 +0330

    End of section 9

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -9,5 +9,6 @@ namespace DatingApp.API.Data
         
         public DbSet<Value> Vlues {get; set;}
         public DbSet<User> Users {get; set;}
+        public DbSet<Photo> Photos { get; set; }
     }
 }
